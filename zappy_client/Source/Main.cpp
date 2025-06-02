f4952c8303d39c5c33b6b7c3e5037d29a31cd53d
Gallini <enzo.gallini@epitech.eu>
2025-06-02 18:02:36 +0200

    feat(gui): Default main file

---
+++ zappy_client/Source/Main.cpp
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2025
+** zappy
+** File description:
+** Main
+*/
+#include "Macro.hpp"
+
+int main(int argc, [[maybe_unused]] char const *argv[])
+{
+    if (argc != GUIMacro::ARG_NUMBER)
+        return GUIMacro::EXIT_FAILLURE;
+    return GUIMacro::EXIT_SUCCESS;
+}
