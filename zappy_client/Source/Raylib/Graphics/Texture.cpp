67e9bcd21f3fd1550564d55878ade164ee1c3794
Gallini <enzo.gallini@epitech.eu>
2025-06-03 11:03:28 +0200

    feat(gui/Raylib): Texture's raylib function encapsulation

---
+++ zappy_client/Source/Raylib/Graphics/Texture.cpp
@@ -0,0 +1,52 @@
+/*
+** EPITECH PROJECT, 2025
+** zappy
+** File description:
+** Texture
+*/
+
+#include "Raylib/Graphics/Texture.hpp"
+
+namespace Raylib {
+namespace Graphics {
+
+Texture::Texture(const std::string& filePath)
+{
+    texture_ = ::LoadTexture(filePath.c_str());
+    if (!IsReady())
+        throw TextureException("Failed to load texture: " + filePath);
+}
+
+Texture::~Texture() {
+    if (IsReady()) {
+        ::UnloadTexture(texture_);
+    }
+}
+
+const Texture2D& Texture::GetRLTexture() const
+{
+    return texture_;
+}
+
+Texture2D& Texture::GetRLTexture()
+{
+    return texture_;
+}
+
+bool Texture::IsReady() const
+{
+    return texture_.id != 0;
+}
+
+int Texture::GetWidth() const
+{
+    return texture_.width;
+}
+
+int Texture::GetHeight() const
+{
+    return texture_.height;
+}
+
+}
+}
---
+++ zappy_client/Source/Raylib/Graphics/Texture.hpp
@@ -0,0 +1,88 @@
+/*
+** EPITECH PROJECT, 2025
+** zappy
+** File description:
+** Texture
+*/
+
+#pragma once
+
+#include <string>
+#include <stdexcept>
+#include <raylib.h>
+
+namespace Raylib {
+namespace Graphics {
+
+class Texture {
+public:
+/**
+     * @brief Constructs a Texture by loading an image from a specified file path.
+     * @param filePath The path to the image file (e.g., .png, .jpg, .bmp).
+     * @throws std::runtime_error if the texture fails to load.
+     */
+    explicit Texture(const std::string& filePath);
+
+    /**
+     * @brief Destructor. Unloads the Raylib Texture2D resource if it was loaded by this instance.
+     */
+    ~Texture();
+
+    Texture(const Texture&) = delete;
+    Texture& operator=(const Texture&) = delete;
+
+    /**
+     * @brief Gets a const reference to the underlying Raylib Texture2D structure.
+     * @return A const reference to Texture2D.
+     */
+    const Texture2D& GetRLTexture() const;
+
+    /**
+     * @brief Gets a mutable reference to the underlying Raylib Texture2D structure.
+     * Use with caution.
+     * @return A mutable reference to Texture2D.
+     */
+    Texture2D& GetRLTexture();
+
+    /**
+     * @brief Checks if the texture has been successfully loaded and is ready for use.
+     * @return true if the texture's ID is valid, false otherwise.
+     */
+    bool IsReady() const;
+
+    /**
+     * @brief Gets the width of the texture.
+     * @return The texture width in pixels.
+     */
+    int GetWidth() const;
+
+    /**
+     * @brief Gets the height of the texture.
+     * @return The texture height in pixels.
+     */
+    int GetHeight() const;
+private:
+    Texture2D texture_;
+
+    class TextureException : public std::exception {
+    public:
+        /**
+         * @brief Constructs a TextureException with a specified error message.
+         * @param message A string describing the error.
+         */
+        TextureException(const std::string &message) : message_(message) {}
+
+        /**
+         * @brief Returns a C-style character string describing the error.
+         * @return A pointer to a null-terminated string.
+         */
+        const char *what() const noexcept override {
+            return message_.c_str();
+        };
+    private:
+        std::string message_;
+    };
+};
+
+}
+}
