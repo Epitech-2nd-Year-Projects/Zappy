5168d9e6e810d0d8710b35ccf439b3f65f4fde5a
LaurentAliu7 <laurent.aliu@epitech.eu>
2025-06-10 00:11:03 +0200

    chore(gui): add doxygen

--- zappy_client/Source/Entity/Egg/Egg.hpp
+++ zappy_client/Source/Entity/Egg/Egg.hpp
@@ -11,16 +11,45 @@
 #include "GameState/Types/GameTypes.hpp"
 
 namespace GUI {
+/**
+ * @class Egg
+ * @brief Represents an egg entity in the game world
+ * 
+ * The Egg class extends the abstract AEntity base class to implement
+ * egg-specific functionality. It tracks the team association and hatching
+ * state of eggs in the game world.
+ */
 class Egg : public AEntity {
     public:
+        /**
+         * @brief Constructs an Egg object
+         * @param id Unique identifier for the egg
+         * @param position Position coordinates of the egg in the game world
+         * @param teamName The team name this egg belongs to
+         * @param orientation Initial orientation of the egg (defaults to NORTH)
+         */
         Egg(uint32_t id, Types::Position position, std::string &teamName,
             Types::Orientation orientation = Types::Orientation::NORTH);
+        
+        /**
+         * @brief Virtual destructor for the Egg class
+         */
         ~Egg() = default;
 
+        /**
+         * @brief Set the hatched state of the egg
+         * @param hatched True if the egg has hatched, false otherwise
+         */
         void setHatched(bool hatched);
+        
+        /**
+         * @brief Check if the egg has hatched
+         * @return True if the egg has hatched, false otherwise
+         */
         bool isHatched() const;
+        
     protected:
-        std::string m_teamName;
-        bool m_hatched;
+        std::string m_teamName; ///< The team name this egg belongs to
+        bool m_hatched;         ///< Flag indicating if the egg has hatched
 };
 }
--- zappy_client/Source/EventManager/EventBus.hpp
+++ zappy_client/Source/EventManager/EventBus.hpp
@@ -14,14 +14,40 @@
 
 namespace GUI {
 namespace EventManager {
+
+/**
+ * @class EventBus
+ * @brief Central event bus for publishing and subscribing to game events
+ *
+ * The EventBus provides a publish-subscribe mechanism for game components
+ * to communicate without direct dependencies. Components can subscribe to
+ * specific event types and receive notifications when those events are published.
+ */
 class EventBus {
     public:
+        /**
+         * @brief Type alias for event handler functions
+         * @tparam EventType The specific event type this handler processes
+         */
         template<typename EventType>
         using Handler = std::function<void(const EventType &)>;
 
+        /**
+         * @brief Default constructor
+         */
         EventBus() = default;
+
+        /**
+         * @brief Default destructor
+         */
         ~EventBus() = default;
 
+        /**
+         * @brief Subscribes a handler function to a specific event type
+         * 
+         * @tparam EventType The type of event to subscribe to (must inherit from IEvent)
+         * @param handler Function to call when the event is published
+         */
         template<typename EventType>
         void subscribe(Handler<EventType> handler)
         {
@@ -34,6 +60,12 @@ class EventBus {
             _subscribers[typeId].push_back(wrapper);
         }
 
+        /**
+         * @brief Publishes an event to all subscribed handlers
+         * 
+         * @tparam EventType The type of event being published
+         * @param event The event object to publish
+         */
         template<typename EventType>
         void publish(const EventType &event) const
         {
@@ -45,8 +77,17 @@ class EventBus {
             }
         }
     private:
+        /**
+         * @brief Type alias for internal event handler functions
+         * Handles events through the common IEvent base interface
+         */
         using EventHandler = std::function<void(const IEvent &)>;
+        
+        /**
+         * @brief Map of event type to vector of handlers
+         * Each event type (identified by type_index) has a vector of handlers that process it
+         */
         std::unordered_map<std::type_index, std::vector<EventHandler>> _subscribers;
 };
-}
-}
+} // namespace EventManager
+} // namespace GUI
