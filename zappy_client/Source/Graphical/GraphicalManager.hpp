c33788a194efad065235893ee2b5a547c32297e6
Gallini <enzo.gallini@epitech.eu>
2025-06-09 09:35:56 +0200

    feat(gui/Graphical): Init GraphicalManager header file

---
+++ zappy_client/Source/Graphical/GraphicalManager.hpp
@@ -0,0 +1,54 @@
+/*
+** EPITECH PROJECT, 2025
+** zappy
+** File description:
+** GraphicalManager
+*/
+
+#pragma once
+
+#include <cstdio>
+#include <string>
+#include "Raylib/Core/Window.hpp"
+#include "Raylib/Graphics/Camera.hpp"
+
+namespace GUI {
+
+    constexpr std::size_t DEFAULT_WINDOW_WIDTH = 1920;
+    constexpr std::size_t DEFAULT_WINDOW_HEIGHT = 1080;
+
+class GraphicalManager {
+public:
+
+    GraphicalManager();
+    ~GraphicalManager();
+
+private:
+    std::string m_windowName;
+    std::size_t m_windowWidth;
+    std::size_t m_windowHeight;
+
+    Raylib::Core::Window m_window;
+    Raylib::Graphics::Camera m_camera;
+
+
+    class GraphicalManagerException : public std::exception {
+    public:
+        /**
+         * @brief Constructs a GraphicalManagerException with a specified error message.
+         * @param message A string describing the error.
+         */
+        GraphicalManagerException(const std::string &message) : message_(message) {}
+
+        /**
+         * @brief Returns a C-style character string describing the error.
+         * @return A pointer to a null-terminated string.
+         */
+        const char *what() const noexcept override {
+            return message_.c_str();
+        };
+    private:
+        std::string message_;
+    };
+};
+}
