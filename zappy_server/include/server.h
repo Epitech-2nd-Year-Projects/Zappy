e066c0d438eed0ea707e80136d50560d1bd5157b
gregorsternat <gregor.sternat@epitech.eu>
2025-06-09 22:37:27 +0200

    feat(server): extend client structure with type and buffer fields

--- zappy_server/include/server.h
+++ zappy_server/include/server.h
@@ -12,9 +12,21 @@
 
     #define IDLE_TIMEOUT 30
 
+    #define BUFFER_SIZE 1024
+
+typedef enum client_type_e {
+    CLIENT_TYPE_UNKNOWN = 0,
+    CLIENT_TYPE_AI,
+    CLIENT_TYPE_GUI
+} client_type_t;
+
 typedef struct client_s {
     int fd;
     time_t last_active;
+    client_type_t type;
+    char *team_name;
+    char input_buffer[BUFFER_SIZE];
+    size_t buffer_pos;
 } client_t;
 
 typedef struct server_s {
