e7d629a6f7d04df9fd98eaf9cde87a7041155ffb
gregorsternat <gregor.sternat@epitech.eu>
2025-06-13 17:36:03 +0200

    feat(server): add handshake header file for client-server interaction

---
+++ zappy_server/include/handshake.h
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2025
+** zappy_server
+** File description:
+** handshake
+*/
+
+#ifndef HANDSHAKE_H
+    #define HANDSHAKE_H
+
+    #include "server.h"
+
+bool handshake_process(server_t *server, client_t *client);
+
+#endif
